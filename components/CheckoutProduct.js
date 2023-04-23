b590d1e7f9470d1e7070650e292aa294dd3e2b02
PrathamD16 <pratham.doke.1602@gmail.com>
2023-04-23 17:22:10 +0530

    Add functionality

--- components/CheckoutProduct.js
+++ components/CheckoutProduct.js
@@ -3,6 +3,8 @@ import Image from "next/image";
 import { StarIcon } from "@heroicons/react/24/solid";
 import { CurrencyDollarIcon } from "@heroicons/react/24/solid";
 import * as CurrencyFormat from "react-currency-format";
+import { useDispatch } from "react-redux";
+import { addToBasket } from "@/slices/basketSlice";
 
 function CheckoutProduct({
   id,
@@ -14,50 +16,74 @@ function CheckoutProduct({
   image,
   hasPrime,
 }) {
-  return (
-    <div className="grid grid-cols-5">
-      <Image
-        src={image}
-        height={200}
-        width={200}
-        style={{ objectFit: "contain" }}
-      />
-      {/* Middle */}
-      <div className="col-span-3 mx-5">
-        <p>{title}</p>
-        <div className="flex ">
-          {Array(rating)
-            .fill()
-            .map((_, i) => (
-              <StarIcon key={i} className="h-5 text-yellow-500" />
-            ))}
-        </div>
-        <p className="text-xs my-2 line-clamp-3">{description}</p>
+  const dispatch = useDispatch();
+  const addtoBasket = () => {
+    const newProduct = {
+      id,
+      title,
+      price,
+      rating,
+      description,
+      category,
+      image,
+      hasPrime,
+    };
+    dispatch(addToBasket(newProduct));
+  };
 
-        <div className="flex mb-5 space-x-3">
-          <CurrencyDollarIcon className="h-5" />
-          <CurrencyFormat
-            className="bg-gray-50"
-            value={price}
-            thousandSeparator={true}
-            disabled
-          />
-        </div>
-        {hasPrime && (
-          <div className="flex items-center space-x-2">
-            <img className="w-12 h-12" loading="lazy" src="https://links.papareact.com/fdw" alt="" />
-            <p className="text-xs text-gray-500">FREE Next-day Delivery</p>
+  {
+    return (
+      <div className="grid grid-cols-5">
+        <Image
+          src={image}
+          height={200}
+          width={200}
+          style={{ objectFit: "contain" }}
+        />
+        {/* Middle */}
+        <div className="col-span-3 mx-5">
+          <p>{title}</p>
+          <div className="flex ">
+            {Array(rating)
+              .fill()
+              .map((_, i) => (
+                <StarIcon key={i} className="h-5 text-yellow-500" />
+              ))}
           </div>
-        )}
-      </div>
+          <p className="text-xs my-2 line-clamp-3">{description}</p>
 
-      {/* Add and remove button */}
-      <div className="flex flex-col space-y-2 my-auto justify-self-end">
-        <button className="button">Add to Basket</button>
-        <button className="button">Remove from basket</button>
+          <div className="flex mb-5 space-x-3">
+            <CurrencyDollarIcon className="h-5" />
+            <CurrencyFormat
+              className="bg-gray-50"
+              value={price}
+              thousandSeparator={true}
+              disabled
+            />
+          </div>
+          {hasPrime && (
+            <div className="flex items-center space-x-2">
+              <img
+                className="w-12 h-12"
+                loading="lazy"
+                src="https://links.papareact.com/fdw"
+                alt=""
+              />
+              <p className="text-xs text-gray-500">FREE Next-day Delivery</p>
+            </div>
+          )}
+        </div>
+
+        {/* Add and remove button */}
+        <div className="flex flex-col space-y-2 my-auto justify-self-end">
+          <button onClick={addtoBasket} className="button">
+            Add to Basket
+          </button>
+          <button className="button">Remove from basket</button>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default CheckoutProduct;
--- slices/basketSlice.js
+++ slices/basketSlice.js
@@ -18,6 +18,6 @@ export const basketSlice = createSlice({
 export const { addToBasket, removeFromBasket } = basketSlice.actions;
 
 // Selectors - This is how we pull information from the Global store slice
-export const selectItems = (state) => state.basket.items;s
+export const selectItems = (state) => state.basket.items;
 
 export default basketSlice.reducer;
